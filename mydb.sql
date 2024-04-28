449243f0534054e436150d0d12a0cc69047afc5f
Jackylin2845 <164992575+Jackylin2845@users.noreply.github.com>
2024-04-28 22:55:22 +1000

    update

--- mydb.sql
+++ mydb.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50716
  File Encoding         : 65001
 
- Date: 31/03/2024 13:11:57
+ Date: 27/04/2024 14:11:02
 */
 
 SET NAMES utf8mb4;
@@ -30,7 +30,7 @@ CREATE TABLE `activities`  (
   `module_id_1` bigint(20) DEFAULT NULL,
   `introduction` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of activities
@@ -41,6 +41,7 @@ INSERT INTO `activities` VALUES (3, 'test activititie2', 'test', '1', 1, 1, 'tes
 INSERT INTO `activities` VALUES (4, 'Activity33', '', NULL, 1, 1, NULL);
 INSERT INTO `activities` VALUES (5, 'aaa', '', NULL, 1, 1, NULL);
 INSERT INTO `activities` VALUES (6, 'ggg', '', NULL, 1, 1, NULL);
+INSERT INTO `activities` VALUES (7, 'tttt', '', NULL, 1, 1, NULL);
 
 -- ----------------------------
 -- Table structure for attribute
@@ -117,6 +118,8 @@ INSERT INTO `menu` VALUES (15, 'report', NULL, NULL, '/report', '/report');
 INSERT INTO `menu` VALUES (16, 'module', NULL, NULL, '/module', '/module');
 INSERT INTO `menu` VALUES (17, 'skill logs', NULL, NULL, '/skill', '/skill');
 INSERT INTO `menu` VALUES (18, 'session', NULL, NULL, '/session', '/session');
+INSERT INTO `menu` VALUES (19, 'menber', NULL, NULL, '/member', '/member');
+INSERT INTO `menu` VALUES (20, 'SysLog', NULL, NULL, '/SysLog', '/SysLog');
 
 -- ----------------------------
 -- Table structure for modules
@@ -185,6 +188,8 @@ INSERT INTO `role_menu` VALUES (15, 'admin');
 INSERT INTO `role_menu` VALUES (16, 'admin');
 INSERT INTO `role_menu` VALUES (17, 'admin');
 INSERT INTO `role_menu` VALUES (18, 'admin');
+INSERT INTO `role_menu` VALUES (19, 'admin');
+INSERT INTO `role_menu` VALUES (20, 'admin');
 
 -- ----------------------------
 -- Table structure for session_activities
@@ -267,6 +272,44 @@ INSERT INTO `sessions` VALUES (2, 'bbb', '2024-03-15', '11:13:00', '11:13:04', '
 INSERT INTO `sessions` VALUES (8, 'session name1', '2024-03-06', NULL, NULL, '12', '12', NULL, 'finished', 'session name1', 3);
 
 -- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `userid` int(10) DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `createtime` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_log
+-- ----------------------------
+INSERT INTO `sys_log` VALUES (6, 4, 'added the role of caseWorker, this name is dsg at 2024-04-27 13:51:29', '2024-04-27 13:51:29');
+INSERT INTO `sys_log` VALUES (7, 4, 'modified the user whose 16, at 2024-04-27 13:51:39', '2024-04-27 13:51:39');
+INSERT INTO `sys_log` VALUES (8, 4, 'deletes user 17, at 2024-04-27 13:54:45', '2024-04-27 13:54:45');
+INSERT INTO `sys_log` VALUES (9, 4, 'added the role of caseWorker, this name is ga at 2024-04-27 14:05:50', '2024-04-27 14:05:50');
+INSERT INTO `sys_log` VALUES (10, 4, 'added the role of caseWorker, this name is ga22 at 2024-04-27 14:05:55', '2024-04-27 14:05:55');
+INSERT INTO `sys_log` VALUES (11, 4, 'added the role of admin, this name is sdgh at 2024-04-27 14:05:59', '2024-04-27 14:05:59');
+INSERT INTO `sys_log` VALUES (12, 4, 'added the role of caseWorker, this name is sadasf at 2024-04-27 14:06:03', '2024-04-27 14:06:03');
+INSERT INTO `sys_log` VALUES (13, 4, 'added the role of admin, this name is asdddd at 2024-04-27 14:06:08', '2024-04-27 14:06:08');
+INSERT INTO `sys_log` VALUES (14, 4, 'added the role of user, this name is 2asd2a at 2024-04-27 14:06:13', '2024-04-27 14:06:13');
+INSERT INTO `sys_log` VALUES (15, 4, 'modified the user whose 31, at 2024-04-27 14:06:19', '2024-04-27 14:06:19');
+INSERT INTO `sys_log` VALUES (16, 4, 'modified the user whose 31, at 2024-04-27 14:06:23', '2024-04-27 14:06:23');
+INSERT INTO `sys_log` VALUES (17, 4, 'modified the user whose 31, at 2024-04-27 14:06:25', '2024-04-27 14:06:25');
+INSERT INTO `sys_log` VALUES (18, 4, 'modified the user whose 31, at 2024-04-27 14:06:28', '2024-04-27 14:06:28');
+INSERT INTO `sys_log` VALUES (19, 4, 'modified the user whose 29, at 2024-04-27 14:06:30', '2024-04-27 14:06:30');
+INSERT INTO `sys_log` VALUES (20, 4, 'added the role of admin, this name is asd3233 at 2024-04-27 14:06:37', '2024-04-27 14:06:37');
+INSERT INTO `sys_log` VALUES (21, 4, 'deletes user 32, at 2024-04-27 14:06:40', '2024-04-27 14:06:40');
+INSERT INTO `sys_log` VALUES (22, 30, 'added the role of user, this name is sss at 2024-04-27 14:08:19', '2024-04-27 14:08:19');
+INSERT INTO `sys_log` VALUES (23, 30, 'added the role of user, this name is ffa at 2024-04-27 14:08:26', '2024-04-27 14:08:26');
+INSERT INTO `sys_log` VALUES (24, 30, 'modified the user whose 33, at 2024-04-27 14:08:33', '2024-04-27 14:08:33');
+INSERT INTO `sys_log` VALUES (25, 30, 'modified the user whose 33, at 2024-04-27 14:08:38', '2024-04-27 14:08:38');
+INSERT INTO `sys_log` VALUES (26, 30, 'modified the user whose 33, at 2024-04-27 14:08:48', '2024-04-27 14:08:48');
+INSERT INTO `sys_log` VALUES (27, 30, 'deletes user 33, at 2024-04-27 14:08:57', '2024-04-27 14:08:57');
+
+-- ----------------------------
 -- Table structure for tips
 -- ----------------------------
 DROP TABLE IF EXISTS `tips`;
@@ -289,7 +332,7 @@ CREATE TABLE `users`  (
   `avatar_i_d` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `avatarID` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 35 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
@@ -298,5 +341,25 @@ INSERT INTO `users` VALUES (1, 'gzp', 'gzp', 'user', NULL, NULL);
 INSERT INTO `users` VALUES (2, '12', '1', 'user', NULL, NULL);
 INSERT INTO `users` VALUES (3, 'tom', 'tom', 'caseWorker', NULL, NULL);
 INSERT INTO `users` VALUES (4, 'admin', 'admin', 'admin', NULL, NULL);
+INSERT INTO `users` VALUES (5, 'ads', 'asd', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (6, 'a1ds', 'a1sd', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (11, 'b2', 'b', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (12, 'as', 'asd', 'user', NULL, '0');
+INSERT INTO `users` VALUES (13, 'asd', 'asd', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (18, 'gas', 'ad', 'admin', NULL, '0');
+INSERT INTO `users` VALUES (19, 'gas2', 'ad', 'admin', NULL, '0');
+INSERT INTO `users` VALUES (20, 'af', 'fdf', 'admin', NULL, '0');
+INSERT INTO `users` VALUES (21, 'ad', 'ff', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (22, 'ww', 'ww', 'user', NULL, '0');
+INSERT INTO `users` VALUES (23, 'asfasfg', 'asgas', 'admin', NULL, '0');
+INSERT INTO `users` VALUES (24, 'asfgg', 'fgg', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (25, 'dsg', 'hg', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (26, 'ga', 'aas', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (27, 'ga22', 'sagas', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (28, 'sdgh', 'sdsg', 'admin', NULL, '0');
+INSERT INTO `users` VALUES (29, 'sadasf', 'sagasd', 'caseWorker', NULL, '0');
+INSERT INTO `users` VALUES (30, 'asdddd', 'asd', 'admin', NULL, '0');
+INSERT INTO `users` VALUES (31, '2asd2a2', 'asfaat3', 'user', NULL, '0');
+INSERT INTO `users` VALUES (34, 'ffa', 'ff', 'user', NULL, '0');
 
 SET FOREIGN_KEY_CHECKS = 1;
